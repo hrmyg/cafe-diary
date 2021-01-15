f4ef757acd0be11178f66a70b36b43d7a5ab39fb
hrmyg <75000880+hrmyg@users.noreply.github.com>
2021-01-15 18:22:52 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: ture, length: { maximum: 50 }
+  validates :email, format: {with: /\A[\w+-.]+@[a-z\d-.]+.[a-z]+\z/i}
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i}
+  end
 end
