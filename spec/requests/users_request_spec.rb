aadfa155656a3dd9f68a71cae64a2552bd3a3312
hrmyg <75000880+hrmyg@users.noreply.github.com>
2021-01-18 19:02:13 +0900

    モデルテスト

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-
+  it '正常なレスポンスを返す' do
+    get new_user_registration_path
+    expect(response).to have_http_status 200
+  end
+  it '正常なレスポンスを返す' do
+    get new_user_session_path
+    expect(response).to have_http_status 200
+  end
 end
