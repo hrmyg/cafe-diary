b81d61e622a00d29893bb12d3c1a80ba5d672faa
hrmyg <75000880+hrmyg@users.noreply.github.com>
2021-01-18 19:29:46 +0900

    バリデーション

--- app/models/cafe.rb
+++ app/models/cafe.rb
@@ -1,4 +1,11 @@
 class Cafe < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  with_options presece: true do
+    validates :name
+    validates :text
+    validates :place
+    validates :image
+  end
 end
